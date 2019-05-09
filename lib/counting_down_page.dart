0fc9e2e8522d1ad9cb0bd770f36621c144f60200
Yongqiang Li <jasonleehust@gmail.com>
2019-05-09 00:11:12 -0700

    Implement a simple timer.

--- lib/counting_down_page.dart
+++ lib/counting_down_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:common_utils/common_utils.dart';
 import 'domains.dart';
+import 'dart:async';
 
 class CountingDownPage extends StatefulWidget {
 
@@ -15,15 +15,51 @@ class CountingDownPage extends StatefulWidget {
 
 class _CountingDownPageState extends State<CountingDownPage> {
 
+  Process _process;
+
+  String totalLeftTime = "";
+
+  DevelopStep currentStep;
+
+  var currentStepDuration = Duration();
+
   @override
   Widget build(BuildContext context) {
+    this._process = widget.theProcess;
+
     var backBtn = IconButton(icon: new Icon(Icons.arrow_back), onPressed: _onBackPressed);
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.theProcess.name),
+        title: Text(_process.name),
       ),
-      body: Text(
-        widget.theProcess.name
+      body: Card(
+        margin: EdgeInsets.all(10.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.lightBlue,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 5.0
+              ),
+            ],
+          ),
+          child: ListTile(
+            title: Text(
+              "${currentStep.name}\n$currentStepDuration",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+              ),
+            ),
+            leading: Icon(Icons.timer, color: Colors.white, size: 40.0,),
+            subtitle: Text(
+              totalLeftTime,
+              style: TextStyle(color: Colors.white, fontSize: 20.0),
+            ),
+          )
+        )
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.pause, color: Colors.white),
@@ -31,6 +67,39 @@ class _CountingDownPageState extends State<CountingDownPage> {
         tooltip: "Pause",
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    const oneSec = const Duration(seconds: 1);
+    var totalDuration = widget.theProcess.duration;
+
+    var stepIndex = 0;
+    currentStep = widget.theProcess.steps[stepIndex];
+    currentStepDuration = currentStep.duration;
+
+    new Timer.periodic(oneSec, (Timer t) {
+      totalDuration -= oneSec;
+      currentStepDuration -= oneSec;
+
+      if (totalDuration < Duration(seconds: 0)) {
+        t.cancel();
+      } else {
+        if (currentStepDuration < Duration(seconds: 0)) {
+          stepIndex++;
+          currentStep = widget.theProcess.steps[stepIndex];
+          currentStepDuration = currentStep.duration - Duration(seconds: 1);
+        }
+
+        setState(() {
+          totalLeftTime = "total left time is " + totalDuration.toString();
+        });
+      }
+
+    });
+
   }
 
 
